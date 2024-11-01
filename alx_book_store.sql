fa831976512324149638cb0c36d074aacf9f6690
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-11-01 21:03:44 +0200

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books(
+book_id INT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT REFERENCES Authors(author_id),
+price DOUBLE,
+publication_date DATE
+);
+
+CREATE TABLE Authors(
+author_id INT PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders(
+order_id INT PRIMARY KEY,
+customer_id INT REFERENCES Customers(customer_id),
+order_date DATE
+);
+
+CREATE TABLE Order_Details(
+orderdetailid INT PRIMARY KEY,
+order_id INT REFERENCES order_id(order_id),
+book_id INT REFERENCES Books(book_id),
+quantity DOUBLE
+);
