4813cd210227019c6616b5d13d3569e184cf454c
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-11-01 22:28:04 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_database():
         
         print("Database 'alx_book_store' created successfully!")
         
-    except mysql.Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
         
     finally:
